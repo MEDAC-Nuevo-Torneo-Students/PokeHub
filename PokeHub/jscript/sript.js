79d3e40d6c71732d5e31fb2f32327649f4856499
MonforteGG <monfortegg@gmail.com>
2024-03-22 14:01:33 +0100

    Fix Import header and footer con JS

--- PokeHub/jscript/sript.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("circle").addEventListener("click", function() {
-    document.querySelector('.header').classList.toggle("active");
-    document.querySelector('.low').classList.toggle("active");
-    document.querySelector('#logo').classList.toggle("active");
-    document.querySelector('#copyright').classList.toggle("active");
-});
