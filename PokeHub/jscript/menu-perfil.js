c9509e546e1aecf29a8fd8640ff761a68b455ba5
jaimegarcia-cmd <jaimegarciagr@gmail.com>
2024-04-27 17:35:47 +0200

    Arreglo icono de perfil

--- PokeHub/jscript/menu-perfil.js
+++ PokeHub/jscript/menu-perfil.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         const iconperfil = document.querySelector('.icons-user');
-       
+        const dropdown = document.querySelector('.dropdown');
 
         iconperfil.addEventListener('click', () => {
-            iconperfil.classList.toggle('show');
-            
+            iconperfil.classList.toggle('active');
+            dropdown.classList.toggle('active');
         });
-    }, 200); // Espera 50 ms antes de ejecutar la función
+
+        // Detener la propagación del evento de clic en los elementos secundarios del dropdown
+        dropdown.addEventListener('click', (event) => {
+            event.stopPropagation();
+        });
+    }, 50); // Espera 50 ms antes de ejecutar la función
 });
