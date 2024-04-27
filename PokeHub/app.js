6c7d18923df5b080f2267b317f6401ffd0f80474
pabrammue <pabrammue@alum.us.es>
2024-04-27 19:06:26 +0200

    Muestra de la feed a traves de la base de datos

--- PokeHub/app.js
+++ PokeHub/app.js
@@ -15,39 +15,65 @@ const connection = require('./db');
 
 // Ruta para renderizar el archivo EJS con los datos de la base de datos
 app.get('/home', (req, res) => {
-    // Realizar consultas a la base de datos para cada Pokémon en el equipo
-    const queries = [ 
-        'select team.id, team.team_name, pokemon_build.* from team left join pokemon_build on team.id_Pokemon1_build = pokemon_build.id union all select team.id, team.team_name, pokemon_build.* from team left join pokemon_build on team.id_Pokemon2_build = pokemon_build.id union all select team.id, team.team_name, pokemon_build.* from team left join pokemon_build on team.id_Pokemon3_build = pokemon_build.id union all select team.id, team.team_name, pokemon_build.* from team left join pokemon_build on team.id_Pokemon4_build = pokemon_build.id union all select team.id, team.team_name, pokemon_build.* from team left join pokemon_build on team.id_Pokemon5_build = pokemon_build.id union all select team.id, team.team_name, pokemon_build.* from team left join pokemon_build on team.id_Pokemon6_build = pokemon_build.id;'
-    ];
+    // Consulta SQL con el prefijo en el nombre de la columna
+    const sqlQuery = `SELECT t.*, 
+                        pb1.id AS Pokemon1_id, pb1.id_pokemon AS Pokemon1_id_pokemon, pb1.gender AS Pokemon1_gender, 
+                        pb1.id_ability AS Pokemon1_id_ability, pb1.id_item AS Pokemon1_id_item, pb1.id_nature AS Pokemon1_id_nature, 
+                        pb1.id_move_1 AS Pokemon1_id_move_1, pb1.id_move_2 AS Pokemon1_id_move_2, pb1.id_move_3 AS Pokemon1_id_move_3, 
+                        pb1.id_move_4 AS Pokemon1_id_move_4, pb1.hp_ivs AS Pokemon1_hp_ivs, pb1.atk_ivs AS Pokemon1_atk_ivs, 
+                        pb1.def_ivs AS Pokemon1_def_ivs, pb1.spa_ivs AS Pokemon1_spa_ivs, pb1.spd_ivs AS Pokemon1_spd_ivs, 
+                        pb1.spe_ivs AS Pokemon1_spe_ivs, 
+                        pb2.id AS Pokemon2_id, pb2.id_pokemon AS Pokemon2_id_pokemon, pb2.gender AS Pokemon2_gender, 
+                        pb2.id_ability AS Pokemon2_id_ability, pb2.id_item AS Pokemon2_id_item, pb2.id_nature AS Pokemon2_id_nature, 
+                        pb2.id_move_1 AS Pokemon2_id_move_1, pb2.id_move_2 AS Pokemon2_id_move_2, pb2.id_move_3 AS Pokemon2_id_move_3, 
+                        pb2.id_move_4 AS Pokemon2_id_move_4, pb2.hp_ivs AS Pokemon2_hp_ivs, pb2.atk_ivs AS Pokemon2_atk_ivs, 
+                        pb2.def_ivs AS Pokemon2_def_ivs, pb2.spa_ivs AS Pokemon2_spa_ivs, pb2.spd_ivs AS Pokemon2_spd_ivs, 
+                        pb2.spe_ivs AS Pokemon2_spe_ivs, 
+                        pb3.id AS Pokemon3_id, pb3.id_pokemon AS Pokemon3_id_pokemon, pb3.gender AS Pokemon3_gender, 
+                        pb3.id_ability AS Pokemon3_id_ability, pb3.id_item AS Pokemon3_id_item, pb3.id_nature AS Pokemon3_id_nature, 
+                        pb3.id_move_1 AS Pokemon3_id_move_1, pb3.id_move_2 AS Pokemon3_id_move_2, pb3.id_move_3 AS Pokemon3_id_move_3, 
+                        pb3.id_move_4 AS Pokemon3_id_move_4, pb3.hp_ivs AS Pokemon3_hp_ivs, pb3.atk_ivs AS Pokemon3_atk_ivs, 
+                        pb3.def_ivs AS Pokemon3_def_ivs, pb3.spa_ivs AS Pokemon3_spa_ivs, pb3.spd_ivs AS Pokemon3_spd_ivs, 
+                        pb3.spe_ivs AS Pokemon3_spe_ivs, 
+                        pb4.id AS Pokemon4_id, pb4.id_pokemon AS Pokemon4_id_pokemon, pb4.gender AS Pokemon4_gender, 
+                        pb4.id_ability AS Pokemon4_id_ability, pb4.id_item AS Pokemon4_id_item, pb4.id_nature AS Pokemon4_id_nature, 
+                        pb4.id_move_1 AS Pokemon4_id_move_1, pb4.id_move_2 AS Pokemon4_id_move_2, pb4.id_move_3 AS Pokemon4_id_move_3, 
+                        pb4.id_move_4 AS Pokemon4_id_move_4, pb4.hp_ivs AS Pokemon4_hp_ivs, pb4.atk_ivs AS Pokemon4_atk_ivs, 
+                        pb4.def_ivs AS Pokemon4_def_ivs, pb4.spa_ivs AS Pokemon4_spa_ivs, pb4.spd_ivs AS Pokemon4_spd_ivs, 
+                        pb4.spe_ivs AS Pokemon4_spe_ivs, 
+                        pb5.id AS Pokemon5_id, pb5.id_pokemon AS Pokemon5_id_pokemon, pb5.gender AS Pokemon5_gender, 
+                        pb5.id_ability AS Pokemon5_id_ability, pb5.id_item AS Pokemon5_id_item, pb5.id_nature AS Pokemon5_id_nature, 
+                        pb5.id_move_1 AS Pokemon5_id_move_1, pb5.id_move_2 AS Pokemon5_id_move_2, pb5.id_move_3 AS Pokemon5_id_move_3, 
+                        pb5.id_move_4 AS Pokemon5_id_move_4, pb5.hp_ivs AS Pokemon5_hp_ivs, pb5.atk_ivs AS Pokemon5_atk_ivs, 
+                        pb5.def_ivs AS Pokemon5_def_ivs, pb5.spa_ivs AS Pokemon5_spa_ivs, pb5.spd_ivs AS Pokemon5_spd_ivs, 
+                        pb5.spe_ivs AS Pokemon5_spe_ivs, 
+                        pb6.id AS Pokemon6_id, pb6.id_pokemon AS Pokemon6_id_pokemon, pb6.gender AS Pokemon6_gender, 
+                        pb6.id_ability AS Pokemon6_id_ability, pb6.id_item AS Pokemon6_id_item, pb6.id_nature AS Pokemon6_id_nature, 
+                        pb6.id_move_1 AS Pokemon6_id_move_1, pb6.id_move_2 AS Pokemon6_id_move_2, pb6.id_move_3 AS Pokemon6_id_move_3, 
+                        pb6.id_move_4 AS Pokemon6_id_move_4, pb6.hp_ivs AS Pokemon6_hp_ivs, pb6.atk_ivs AS Pokemon6_atk_ivs, 
+                        pb6.def_ivs AS Pokemon6_def_ivs, pb6.spa_ivs AS Pokemon6_spa_ivs, pb6.spd_ivs AS Pokemon6_spd_ivs, 
+                        pb6.spe_ivs AS Pokemon6_spe_ivs, 
+                        p.likes,
+                        ui.nickname AS nickname
+                        
+                    FROM team t 
+                    INNER JOIN pokemon_build pb1 ON t.id_Pokemon1_build = pb1.id 
+                    INNER JOIN pokemon_build pb2 ON t.id_Pokemon2_build = pb2.id 
+                    INNER JOIN pokemon_build pb3 ON t.id_Pokemon3_build = pb3.id 
+                    INNER JOIN pokemon_build pb4 ON t.id_Pokemon4_build = pb4.id 
+                    INNER JOIN pokemon_build pb5 ON t.id_Pokemon5_build = pb5.id 
+                    INNER JOIN pokemon_build pb6 ON t.id_Pokemon6_build = pb6.id 
+                    INNER JOIN post p ON t.id = p.id_team
+                    INNER JOIN user_info ui ON t.id_user = ui.id`;
+                    
 
-    // Realizar las consultas en serie
-    async.parallel(queries.map(query => {
-        return function(callback) {
-            connection.query(query, (err, results) => {
-                if (err) {
-                    console.error('Error al realizar la consulta:', err);
-                    callback(err);
-                } else {
-                    callback(null, results);
-                }
-            });
-        };
-    }), (err, results) => {
+    connection.query(sqlQuery, (err, results) => {
         if (err) {
-            // Manejar errores
+            console.error('Error al realizar la consulta:', err);
             res.status(500).send('Error interno del servidor');
         } else {
-            // Renderizar el archivo EJS y pasar los resultados de las consultas como datos
-            res.render('home', { 
-                datos: results,
-                datos1: results[0], 
-                datos2: results[1], 
-                datos3: results[2], 
-                datos4: results[3], 
-                datos5: results[4], 
-                datos6: results[5],
-                posts: results[6]
-            });
+            // Renderizar el archivo EJS y pasar los resultados de la consulta como datos
+            res.render('home', { datos: results });
         }
     });
 });
