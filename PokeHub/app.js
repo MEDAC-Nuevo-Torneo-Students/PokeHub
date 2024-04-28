3c02df3eb0c699ef391c1399d936fa1ce1e5365a
MonforteGG <monfortegg@gmail.com>
2024-04-28 17:45:47 +0200

    login and register

--- PokeHub/app.js
+++ PokeHub/app.js
@@ -2,9 +2,17 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
 const async = require('async');
+const Swal = require('sweetalert2');
+
+
+
 
 const app = express();
 const port = 3000;
+
+//2 - steamos urlencoded para capturar los datos del formulario
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 // Configurar EJS como motor de plantillas
 app.set('view engine', 'ejs');
@@ -13,7 +21,7 @@ app.set('views', path.join(__dirname, 'src', 'views'));
 // Importar la conexión a la base de datos
 const connection = require('./db');
 
-// Ruta para renderizar el archivo EJS con los datos de la base de datos
+/*
 app.get('/home', (req, res) => {
     // Realizar consultas a la base de datos para cada Pokémon en el equipo
     const queries = [ 
@@ -55,15 +63,114 @@ app.get('/home', (req, res) => {
             });
         }
     });
-});
+});  */
 
 // Ruta para cargar index.ejs cuando se acceda a /home
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
+
+
+
+
+app.post('/register', async (req,res)=> {
+    const user = req.body.user;
+    const email = req.body.email;
+    const pass = req.body.pass;
+    const description = ""
+    const active = "1"
+    connection.query('INSERT INTO user_info SET ?', {nickname:user, email:email, user_password:pass, description:description, active:active }, async (error,results)=>{
+        if(error) {
+            res.render('register', {
+                alert: true,
+                alertTitle: "Error",
+                alertMessage:"Ya existe un usuario con este username o email",
+                alertIcon:'error',
+                showConfirmButton: false,
+                timer:3500,
+                ruta: 'login'
+            });
+            console.log(error);
+        }else{
+            res.render('register', {
+                alert: true,
+                alertTitle: "Registration",
+                alertMessage:"¡Successful Registration!",
+                alertIcon:'success',
+                showConfirmButton:false,
+                timer:3500,
+                ruta: 'login'
+            });
+        }
+    })
+})
+
+
+//  Auth
+app.post('/auth', async (req,res)=>{
+    const user = req.body.user;
+    const pass = req.body.pass;
+    
+    if(user && pass) {
+        connection.query('SELECT * FROM user_info WHERE nickname = ?', [user], async (error, results)=>{
+            if(error) {
+                console.error(error);
+                res.status(500).send('Error interno del servidor');
+                return;
+            }
+
+            if(results.length === 0 || pass !== results[0].user_password){
+                res.render('login', {
+                    alert: true,
+                    alertTitle: "Error",
+                    alertMessage:"Usuario y/o password incorrectas",
+                    alertIcon:'error',
+                    showConfirmButton: false,
+                    timer:1500,
+                    ruta: 'login'
+                });
+            } else {
+                res.render('login', {
+                    alert: true,
+                    alertTitle: "Connection",
+                    alertMessage:"¡Successful Connection!",
+                    alertIcon:'success',
+                    showConfirmButton: false,
+                    timer:3500,
+                    ruta: 'home'
+                });
+            }
+        });
+    } else {
+        res.status(400).send('Faltan credenciales de usuario o contraseña');
+    }
+});
+
+
+
 // Servir archivos estáticos desde la carpeta raíz
 app.use(express.static(__dirname));
+
+
+
+
+
+// Var. de session
+const session = require('express-session')
+app.use(session({
+    secret:'secret',
+    resave:true,
+    saveUninitialized:true
+}))
 
 // Iniciar el servidor
 app.listen(port, () => {
